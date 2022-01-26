821040547a9320a0eae7ad4dcec35aa2fe473a11
Nazninjhum <nazninnaznin725@gmail.com>
2022-01-26 17:22:15 +0600

    BBC News

---
+++ android/app/src/main/kotlin/com/example/cnn_web_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cnn_web_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Data_Components/Data.dart
@@ -0,0 +1,39 @@
+import 'package:cnn_web_ui/Data_Components/News.dart';
+
+List<String> categories = [
+  'News',
+  'Finance',
+  'Poster',
+  'Job',
+  'Weather',
+  'Catalog',
+  'TV',
+  'TAM.BY',
+];
+
+List<News> news = [
+  News(
+    image: 'assets/6.jpg',
+    category: 'Politics',
+    date: 'JAN 22,2022',
+    h1: 'Trump\'s planned troop\npullout from germany\nquestined',
+  ),
+  News(
+    image: 'assets/2.jpeg',
+    category: 'Politics',
+    date: 'JAN 22,2022',
+    h1: 'Flight carring comatose Russian dissident Navalny lands in Berlin',
+    h2: '. Watch: Flight carring comatose Russian dissident',
+    h3: '. Flight carring comatose Russian dissident Navalny',
+  ),
+  News(
+    image: 'assets/5.jpg',
+    category: 'Sport',
+    date: 'JAN 22,2022',
+    h1: 'Christian Eriksen sends message to Danish teammates from hospital',
+    h2: '. Watch: Denmark’s Eriksen rushed to hospital',
+    h3: '. Eriksen sends message to Danish teammates',
+  ),
+
+
+];
